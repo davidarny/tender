15d8a077bd8465820419cf57a67c6f6c94cca4e3
David Arutiunian <arutunjan666@mail.com>
2019-05-17 10:18:24 +0300

    fix(private route): redirect to base path

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,13 +3,14 @@
 import { jsx } from "@emotion/core";
 import { Redirect } from "@reach/router";
 import PropTypes from "prop-types";
+import { BASE_PATH } from "context";
 
 export default function PrivateRoute({ isLoggedIn, noRedirect = false, render }) {
     if (!isLoggedIn) {
         if (noRedirect) {
             return null;
         } else {
-            return <Redirect to="/login" noThrow />;
+            return <Redirect to={BASE_PATH + "/login"} noThrow />;
         }
     } else {
         return render();
